841dfd2ac20a2e8d270a57e1a68f34b022cd78dc
KenJacopini <kennethgranero@gmail.com>
2020-02-13 15:35:29 +0100

    database script updated

--- spacehubDataAccess/src/main/resources/spacehubDb.sql
+++ spacehubDataAccess/src/main/resources/spacehubDb.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema spacehub
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `spacehub` ;
@@ -15,7 +18,6 @@ DROP SCHEMA IF EXISTS `spacehub` ;
 CREATE SCHEMA IF NOT EXISTS `spacehub` DEFAULT CHARACTER SET utf8 ;
 USE `spacehub` ;
 
-
 -- -----------------------------------------------------
 -- Table `spacehub`.`spacehubUser`
 -- -----------------------------------------------------
@@ -24,50 +26,18 @@ DROP TABLE IF EXISTS `spacehub`.`spacehubUser` ;
 CREATE TABLE IF NOT EXISTS `spacehub`.`spacehubUser` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(100) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `first_name` VARCHAR(45) NULL,
-  `phone_number` VARCHAR(45) NULL,
-  `company_name` VARCHAR(45) NULL,
-  `enabled` TINYINT(1) NULL,
+  `password` VARCHAR(100) NULL DEFAULT NULL,
+  `last_name` VARCHAR(45) NULL DEFAULT NULL,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL,
+  `phone_number` VARCHAR(45) NULL DEFAULT NULL,
+  `company_name` VARCHAR(45) NULL DEFAULT NULL,
+  `enabled` TINYINT(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`, `email`),
-  INDEX `index2` (`email` ASC),
-  UNIQUE INDEX `company_name_UNIQUE` (`company_name` ASC))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `spacehub`.`productStatus`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `spacehub`.`productStatus` ;
-
-CREATE TABLE IF NOT EXISTS `spacehub`.`productStatus` (
-  `productStatusId` INT NOT NULL AUTO_INCREMENT,
-  `productStatusDetail` VARCHAR(45) NULL,
-  PRIMARY KEY (`productStatusId`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `spacehub`.`product`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `spacehub`.`product` ;
-
-CREATE TABLE IF NOT EXISTS `spacehub`.`product` (
-  `product_id` INT NOT NULL AUTO_INCREMENT,
-  `product_name` VARCHAR(45) NULL,
-  `product_plan` VARCHAR(45) NULL,
-  `product_price` DOUBLE NULL,
-  `product_image` VARCHAR(45) NULL,
-  `productStatus_productStatusId` INT NOT NULL,
-  PRIMARY KEY (`product_id`),
-  INDEX `fk_product_productStatus1_idx` (`productStatus_productStatusId` ASC),
-  CONSTRAINT `fk_product_productStatus1`
-    FOREIGN KEY (`productStatus_productStatusId`)
-    REFERENCES `spacehub`.`productStatus` (`productStatusId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  UNIQUE INDEX `company_name_UNIQUE` (`company_name` ASC),
+  INDEX `index2` (`email` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -76,9 +46,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `spacehub`.`organization` ;
 
 CREATE TABLE IF NOT EXISTS `spacehub`.`organization` (
-  `organizationId` INT NOT NULL AUTO_INCREMENT,
-  `organizationName` VARCHAR(100) NULL,
-  `spacehubUser_id` INT NOT NULL,
+  `organizationId` INT(11) NOT NULL AUTO_INCREMENT,
+  `organizationName` VARCHAR(100) NULL DEFAULT NULL,
+  `spacehubUser_id` INT(11) NOT NULL,
   PRIMARY KEY (`organizationId`),
   INDEX `fk_organization_spacehubUser1_idx` (`spacehubUser_id` ASC),
   CONSTRAINT `fk_organization_spacehubUser1`
@@ -86,7 +56,25 @@ CREATE TABLE IF NOT EXISTS `spacehub`.`organization` (
     REFERENCES `spacehub`.`spacehubUser` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `spacehub`.`product`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `spacehub`.`product` ;
+
+CREATE TABLE IF NOT EXISTS `spacehub`.`product` (
+  `product_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `product_name` VARCHAR(45) NULL DEFAULT NULL,
+  `product_plan` VARCHAR(45) NULL DEFAULT NULL,
+  `product_price` DOUBLE NULL DEFAULT NULL,
+  `product_image` VARCHAR(45) NULL DEFAULT NULL,
+  `product_status` VARCHAR(45) NULL,
+  PRIMARY KEY (`product_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -95,27 +83,28 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `spacehub`.`spacehubBooking` ;
 
 CREATE TABLE IF NOT EXISTS `spacehub`.`spacehubBooking` (
-  `book_id` INT NOT NULL AUTO_INCREMENT,
-  `time` DATETIME NULL,
-  `email` VARCHAR(45) NULL,
-  `duration` INT(31) NULL,
-  `product_product_id` INT NOT NULL,
-  `organization_organizationId` INT NOT NULL,
+  `book_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `time` DATETIME NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `duration` INT(31) NULL DEFAULT NULL,
+  `product_product_id` INT(11) NOT NULL,
+  `organization_organizationId` INT(11) NOT NULL,
   PRIMARY KEY (`book_id`),
-  INDEX `fk_spacehubBooking_product1_idx` (`product_product_id` ASC),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  INDEX `fk_spacehubBooking_product1_idx` (`product_product_id` ASC),
   INDEX `fk_spacehubBooking_organization1_idx` (`organization_organizationId` ASC),
-  CONSTRAINT `fk_spacehubBooking_product1`
-    FOREIGN KEY (`product_product_id`)
-    REFERENCES `spacehub`.`product` (`product_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_spacehubBooking_organization1`
     FOREIGN KEY (`organization_organizationId`)
     REFERENCES `spacehub`.`organization` (`organizationId`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_spacehubBooking_product1`
+    FOREIGN KEY (`product_product_id`)
+    REFERENCES `spacehub`.`product` (`product_id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -124,8 +113,8 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `spacehub`.`spacehubRole` ;
 
 CREATE TABLE IF NOT EXISTS `spacehub`.`spacehubRole` (
-  `spacehubRoleId` INT NOT NULL AUTO_INCREMENT,
-  `spacehubRoleName` VARCHAR(45) NULL,
+  `spacehubRoleId` INT(11) NOT NULL AUTO_INCREMENT,
+  `spacehubRoleName` VARCHAR(45) NULL DEFAULT NULL,
   `spacehubUser_email` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`spacehubRoleId`),
   INDEX `fk_spacehubRole_spacehubUser1_idx` (`spacehubUser_email` ASC),
@@ -134,7 +123,8 @@ CREATE TABLE IF NOT EXISTS `spacehub`.`spacehubRole` (
     REFERENCES `spacehub`.`spacehubUser` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
